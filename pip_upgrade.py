adb1d8b828e5e134cc6ecfe7da29379d958fa062
chrifpa <chrifpa@gmail.com>
2014-10-22 15:57:29 +0200

    added script for updating packages installed via pip3

---
+++ pip_upgrade.py
@@ -0,0 +1,7 @@
+#!/usr/local/env python3
+
+import pip
+from subprocess import call
+
+for dist in pip.get_installed_distributions():
+        call("pip3 install --upgrade " + dist.project_name, shell=True)
